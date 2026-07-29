742dcb70a687587399749c3e7c3592f3054521f5
weenastardes <estefanny.febles@gmail.com>
2026-07-29 22:45:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ if "maquinaria" not in st.session_state:
     }
 
 if "historial_finanzas" not in st.session_state:
-    fechas = pd.date_range(end=pd.Timestamp.now(), periods=15, freq="H")
+   fechas = pd.date_range(end=pd.Timestamp.now(), periods=15, freq="h")
     st.session_state.historial_finanzas = pd.DataFrame({
         "Tiempo": fechas,
         "Capital": [150000.0 + i * random.randint(-500, 2000) for i in range(15)],
